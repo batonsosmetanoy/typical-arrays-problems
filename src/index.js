500fe4c8343eeed803667e4520b57c4af87cdabd
batonsosmetanoy <madraimovmsh@gmail.com>
2020-03-20 10:36:19 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || !Array.isArray(array) || array.length === 0) return 0;
+    array.sort((a, b) => a - b);
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || !Array.isArray(array) || array.length === 0) return 0;
+    array.sort((a, b) => a - b);
+    return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || !Array.isArray(array) || array.length === 0) return 0;
+    return array.reduce((sum, current) => sum += current, 0) / array.length;
 }
